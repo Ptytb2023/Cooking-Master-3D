4edacabd6723c043f0b22efaadd7f40946b5a7d9
Vlados <ptytb2015@mail.ru>
2024-09-16 18:57:35 +0500

    Creat GameData

---
+++ Assets/Scripts/Data/GameData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Data
+{
+    [Serializable]
+    public class GameData
+    {
+        [field: SerializeField] public int MaxCustomer { get; set; }
+        [field: SerializeField] public float TimeServeCustomerCashRegister { get; set; }
+        [field: SerializeField] public float TimeServePickupCounter { get; set; }
+        [field: SerializeField] public float TimeCookForGrill { get; set; }
+        [field: SerializeField] public float  PlayerMoveSpeed { get; set; }
+        [field: SerializeField] public Range TimeBeetwenSpawn { get; set; }
+    }
+}
---
+++ Assets/Scripts/Data/Range.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Data
+{
+    [Serializable]
+    public class Range
+    {
+        [field: SerializeField] public float Max { get; private set; }
+        [field: SerializeField] public float Min { get; private set; }
+    }
+}
